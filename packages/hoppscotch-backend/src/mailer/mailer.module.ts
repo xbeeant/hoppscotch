102486b5b9e61ba235658f20f45b0596e98502fd
Beeant <huangxb0512@gmail.com>
2023-10-12 01:59:06 +0000

    fix: mailer

--- packages/hoppscotch-backend/src/mailer/mailer.module.ts
+++ packages/hoppscotch-backend/src/mailer/mailer.module.ts
@@ -3,16 +3,24 @@ import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { MailerService } from './mailer.service';
 import { throwErr } from 'src/utils';
-import {
-  MAILER_FROM_ADDRESS_UNDEFINED,
-  MAILER_SMTP_URL_UNDEFINED,
-} from 'src/errors';
+import { MAILER_FROM_ADDRESS_UNDEFINED } from 'src/errors';
 
 @Module({
   imports: [
     NestMailerModule.forRoot({
-      transport:
-        process.env.MAILER_SMTP_URL ?? throwErr(MAILER_SMTP_URL_UNDEFINED),
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        ignoreTLS: process.env.SMTP_IGNORE_TLS === 'true',
+        secure: process.env.SMTP_SECURE === 'true',
+        tls: {
+          rejectUnauthorized: process.env.SMTP_REJECT_UNAUTHORIZED === 'true',
+        },
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      },
       defaults: {
         from:
           process.env.MAILER_ADDRESS_FROM ??
